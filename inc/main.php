d6ddc549bf660de3168ca5cde28868be935382d7
yannbug <yann@abc.fr>
2016-11-23 12:34:10 +0100

    Initial commit

---
+++ inc/main.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * WordPress Add Menu In Footer plugin main class
+*
+* @author yann@abc.fr
+* @see: https://github.com/Celyan-SAS/wp-add-menu-in-footer
+*
+*/
+class wpCelyanFooterMenu {
+	
+	
+	/**
+	 * Class constructor
+	 *
+	 */
+	public function __construct() {
+		
+	}
+}
+?>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *	@package WordPress Add Menu In Footer
+ *	@author Celyan
+ *	@version 0.0.1
+ */
+/*
+ Plugin Name: WordPress Add Menu In Footer
+ Plugin URI: https://github.com/Celyan-SAS/wp-add-menu-in-footer
+ Description: WordPress plugin to add an additional menu in the footer of a site
+ Version: 0.0.1
+ Author: Yann Dubois
+ Author URI: http://www.yann.com/
+ License: GPL2
+ */
+
+include_once( dirname(__FILE__) . '/inc/main.php' );
+
+new wpCelyanFooterMenu();
