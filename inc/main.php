ba489bbb7195b8aae9d4f4a128ac6a0c5b8228b2
yannbug <yann@abc.fr>
2016-11-23 12:42:35 +0100

    Implement additional footer menu

--- inc/main.php
+++ inc/main.php
@@ -15,6 +15,17 @@ class wpCelyanFooterMenu {
 	 */
 	public function __construct() {
 		
+		add_action( 'after_setup_theme', array( $this, 'register_my_menu' ) );
+		
+		add_action( 'wp_footer', array( $this, 'display_my_menu' ) );
+	}
+	
+	public function register_my_menu() {
+		register_nav_menu( 'footer', __( 'Footer Menu', 'wpcfootermenu' ) );
+	}
+	
+	public function display_my_menu() {
+		wp_nav_menu( array( 'theme_location' => 'footer' ) );
 	}
 }
 ?>
